341b53c573af503fb272a36fc313a82c284974f9
Martin Spacek <git@mspacek.mm.st>
2022-05-04 09:11:43 +0200

    Initial commit (delete history)

---
+++ 12_organizing/example.py
@@ -0,0 +1,11 @@
+def squared(x):
+    """Return the square of x.
+
+    >>> squared(2)
+    4
+    >>> squared(3)
+    9
+    >>> squared(4)
+    16
+    """
+    return x**2
---
+++ 12_organizing/modules.py
@@ -0,0 +1,7 @@
+def times2(x):
+    return x * 2
+
+
+if __name__ == '__main__':
+    print('Hello world')
+    print(times2(2))
